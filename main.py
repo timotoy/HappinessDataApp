5f15a3fbc9e6934690a8d8dadee64eb391ef0e8d
tblentic <tblentic1@gmail.com>
2025-07-13 11:54:19 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="CompareCountries")
+
+st.header("Compare Countries")
+
+df = pd.read_csv("happy.csv")
+
+selectables = ["Happiness", "GDP", "Social support", "Life expectancy",
+                                        "Freedom to make life choices", "Corruption", "Generosity"]
+
+horizontal = st.selectbox("Select horizontal axis", selectables)
+vertical = st.selectbox("Select vertical axis", selectables)
+
+def get_x():
+    for selectable in selectables:
+        if selectable == horizontal:
+            chosen_horizontal = selectable.replace(" ", "_").lower()
+    return chosen_horizontal
+
+def get_y():
+    for selectable in selectables:
+        if selectable == vertical:
+            chosen_vertical = selectable.replace(" ", "_").lower()
+    return chosen_vertical
+
+x = get_x()
+y = get_y()
+
+fig = px.scatter(
+    df,
+    x=x,
+    y=y,
+    hover_name="country",
+    title=f"{vertical} vs {horizontal}",
+    labels={x: horizontal, y: vertical}
+)
+
+st.plotly_chart(fig)
